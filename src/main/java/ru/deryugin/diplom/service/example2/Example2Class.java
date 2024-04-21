cefc5d8bf97a515a2d648f328b4eb694e9831b5c
Максим <maksim_28_11_1999@mail.ru>
2024-04-21 22:31:09 +0300

    minor fix

--- src/main/java/ru/deryugin/diplom/service/example2/Example2Class.java
+++ src/main/java/ru/deryugin/diplom/service/example2/Example2Class.java
@@ -6,8 +6,8 @@ import ru.deryugin.diplom.annotation.LogExecutionTime;
 
 @Component
 public class Example2Class implements Example2Interface {
-    @Override
     @LogExecutionTime
+    @Override
     public void doWork() throws InterruptedException {
         Thread.sleep(500);
         System.out.println("interface method executed");
